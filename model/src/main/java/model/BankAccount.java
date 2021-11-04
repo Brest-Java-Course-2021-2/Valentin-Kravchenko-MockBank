f598de83fb63f63bb5c3cd643afc9dc698b8894e
Valentin Kracvhenko <valvik@hotmail.com>
2021-11-04 09:51:30 +0300

    multi-module structure (model, dao)

---
+++ model/src/main/java/model/BankAccount.java
@@ -0,0 +1,11 @@
+package model;
+
+public class BankAccount {
+
+     private Integer accountId;
+
+     private String accountNumber;
+
+     private String clientFullName;
+
+}
---
+++ model/src/main/java/model/CreditCard.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.math.BigDecimal;
+
+public class CreditCard {
+
+    private Integer cardId;
+
+    private String cardNumber;
+
+    private BigDecimal balance;
+
+    private Integer accountId;
+
+}
