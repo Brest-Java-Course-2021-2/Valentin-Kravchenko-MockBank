609bd4926ded9086b32eac71fb492ea570f17abe
Valentin Kracvhenko <valvik@hotmail.com>
2021-11-05 10:44:56 +0300

    Bank account model, Credit card model

--- model/src/main/java/model/BankAccount.java
+++ model/src/main/java/model/BankAccount.java
@@ -1,11 +1,30 @@
 package model;
 
+import java.time.LocalDate;
+
+/**
+ *  Bank account model
+ */
 public class BankAccount {
 
+     /**
+      *  Bank account ID
+      */
      private Integer accountId;
 
+     /**
+      *  International bank account number
+      */
      private String accountNumber;
 
-     private String clientFullName;
+     /**
+      *  Full name of the bank customer holding the bank account
+      */
+     private String customer;
+
+     /**
+      *  Date of the customer registration in the bank system
+      */
+     private LocalDate registrationDate;
 
 }
--- model/src/main/java/model/CreditCard.java
+++ model/src/main/java/model/CreditCard.java
@@ -1,15 +1,36 @@
 package model;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
+/**
+ *  Credit card model
+ */
 public class CreditCard {
 
+    /**
+     *  Credit card ID
+     */
     private Integer cardId;
 
+    /**
+     *  Credit card number
+     */
     private String cardNumber;
 
+    /**
+     *  Credit card expiration date
+     */
+    private LocalDate expirationDate;
+
+    /**
+     *  Credit card balance
+     */
     private BigDecimal balance;
 
+    /**
+     *  Bank account ID linked with the credit card
+     */
     private Integer accountId;
 
 }
